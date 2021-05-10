bcd58021077220c5af1446c42f5c7be66dba99f0
maniek <mpastuszka@gmail.com>
2021-05-10 19:18:40 +0200

    [init] - project skeleton

---
+++ src/main/java/pl/sda/mybudget/MyBudgetApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.mybudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBudgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/mybudget/MyBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.mybudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
