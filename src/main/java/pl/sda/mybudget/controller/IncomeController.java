a1c62ad2edf6c31675d5f58d471ace29fababc77
maniek <mpastuszka@gmail.com>
2021-05-31 19:53:32 +0200

    [change] - all incomes in html template implemented

--- src/main/java/pl/sda/mybudget/controller/IncomeController.java
+++ src/main/java/pl/sda/mybudget/controller/IncomeController.java
@@ -1,15 +1,29 @@
 package pl.sda.mybudget.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sda.mybudget.service.IncomeService;
 
 @RequestMapping("/income")
 @Controller
 public class IncomeController {
 
+    private final IncomeService incomeService;
+
+    public IncomeController(final IncomeService incomeService) {
+        this.incomeService = incomeService;
+    }
+
     @GetMapping("/all-incomes")
-    public String allIncomes() {
+    public String allIncomes(Model model) {
+        // I - find required data
+        var allIncomes = incomeService.findAllIncomes();
+        // II - add data to model
+        model.addAttribute("incomes", allIncomes);
+
+        // III - template engine takes data and template name and produces html
         return "income/all-incomes";
     }
 }
